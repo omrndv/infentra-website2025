21fb57a3da85084214a8ceed5278779d56f8d342
omrndv <umarnadiv@gmail.com>
2025-11-06 20:30:57 +0700

    fix navbar

--- resources/views/components/frontend/navbar.blade.php
+++ resources/views/components/frontend/navbar.blade.php
@@ -3,14 +3,12 @@
         <a
             class="navbar-brand mx-auto mx-lg-0 d-flex flex-row justify-content-center align-self-center"
             href="{{ route('frontend.landing') }}">
-            href="{{ route('frontend.landing') }}">
             <img
                 src="{{ $appSettings['nav_logo'] ?? '#' }}"
                 alt="Logo"
                 width="60px"
                 height="80px"
-                loading="lazy" />
-                loading="lazy" />
+                loading="lazy"/>
             <div class="d-flex flex-column align-self-center justify-self-center mx-3">
                 <span class="text-judul" style="font-weight: 800;">
                     {{ $appSettings['title'] }}
