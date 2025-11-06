56227525e06d55978fe82579ce1038a3a5c30e37
omrndv <umarnadiv@gmail.com>
2025-11-06 20:37:03 +0700

    ubah margin lupa pw

--- resources/views/pages/auth/passwords/email.blade.php
+++ resources/views/pages/auth/passwords/email.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.auth title="Lupa Password">
-    <div class="page-content d-flex align-items-center justify-content-center">
+    <div class="page-content d-flex align-items-center justify-content-center pt-5 mt-5">
         <div class="row w-100 mx-0 auth-page">
             <div class="col-md-8 col-xl-4 mx-auto">
-                <div class="card">
+                <div class="card" style="border: 4px solid #000; box-shadow: 6px 6px 0 0 #000;">
                     <div class="auth-form-wrapper px-4 ps-5 py-5 pe-5">
                         <h5 class="text-muted fw-normal mb-4">
                             Masukkan email Anda untuk menerima link reset password
